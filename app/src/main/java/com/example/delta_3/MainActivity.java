16e51df2ade401355e488f56b43ee52892c8c0b4
supriya-pd <59952449+supriya-pd@users.noreply.github.com>
2020-08-21 14:31:16 +0530

    second commit

--- app/src/main/java/com/example/delta_3/MainActivity.java
+++ app/src/main/java/com/example/delta_3/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
              @Override
              public void onClick(View view) {
                  customClock.resetButtonClicked();
+                 toggleButton.setChecked(false);
              }
          });
 
